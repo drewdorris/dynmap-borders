37446b6d81d80cceed00bf4c4df3c49a0c4620ad
drewdorris <contact@dorrisd.com>
2020-04-06 00:15:12 -0400

    allow multiple shapefiles, more changes

--- src/main/java/com/ruinscraft/countries/DynmapCountries.java
+++ src/main/java/com/ruinscraft/countries/DynmapCountries.java
@@ -1,12 +1,16 @@
 package com.ruinscraft.countries;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.Reader;
 import java.util.regex.Pattern;
 
 import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.dynmap.DynmapAPI;
@@ -22,6 +26,7 @@ import org.opengis.feature.Property;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
 
+import com.google.common.io.ByteStreams;
 import com.google.common.io.CharStreams;
 
 public class DynmapCountries extends JavaPlugin {
@@ -38,6 +43,8 @@ public class DynmapCountries extends JavaPlugin {
 	private int zOffset = 0;
 
 	private World world;
+
+	FileConfiguration cfg;
 
 	private FeatureSource<SimpleFeatureType, SimpleFeature> featureSource;
 
@@ -65,7 +72,34 @@ public class DynmapCountries extends JavaPlugin {
 		}
 	}
 
-	// handles everything
+	/**
+	 * Used to load config.yml
+	 * @param resource
+	 * @return File
+	 */
+	public File loadResource(String resource) {
+        File folder = getDataFolder();
+        if (!folder.exists())
+            folder.mkdir();
+        File resourceFile = new File(folder, resource);
+        try {
+            if (!resourceFile.exists()) {
+                resourceFile.createNewFile();
+                try (InputStream in = this.getResource(resource);
+                     OutputStream out = new FileOutputStream(resourceFile)) {
+                    ByteStreams.copy(in, out);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return resourceFile;
+    }
+
+	/**
+	 * Handles everything
+	 * @throws IOException
+	 */
 	private void activate() throws IOException {
 		this.markerapi = api.getMarkerAPI();
 		if (this.markerapi == null) {
@@ -73,137 +107,149 @@ public class DynmapCountries extends JavaPlugin {
 			return;
 		}
 
-		FileConfiguration cfg = getConfig();
-		cfg.options().copyDefaults(true);   /* Load defaults, if needed */
-		this.saveConfig();  /* Save updates, if needed */
-
-		if (this.getResource(cfg.getString("countriesFile")) == null) {
-			this.saveResource(cfg.getString("countriesFile"), false);
-		}
-		this.scaling = cfg.getDouble("scaling", 120);
-		this.xOffset = cfg.getInt("xOffset", 0);
-		this.y = cfg.getInt("y", 64);
-		this.zOffset = cfg.getInt("zOffset", 0);
-
-		File shapefile = new File(this.getDataFolder(), this.getConfig().getString("shapefilePath"));
-		if (shapefile == null || !shapefile.isFile()) {
-			this.getLogger().warning("Shapefile not found!");
-			return;
-		}
-
-		FileDataStore store = FileDataStoreFinder.getDataStore(shapefile);
-
-		this.featureSource = store.getFeatureSource();
+		File configFile = this.loadResource("config.yml");
+		this.cfg = YamlConfiguration.loadConfiguration(configFile);
 
 		// Create new countries markerset
 		markerSet = markerapi.getMarkerSet("countries.markerset");
 		if(markerSet == null) {
-			markerSet = markerapi.createMarkerSet("countries.markerset", cfg.getString("style.layerName", "Countries"), null, false);
+			markerSet = markerapi.createMarkerSet("countries.markerset",
+					cfg.getString("layerName", "Countries"), null, false);
 		} else {
-			markerSet.setMarkerSetLabel(cfg.getString("style.layerName", "Countries"));
+			markerSet.setMarkerSetLabel(cfg.getString("layerName", "Countries"));
 		}
 		if (markerSet == null) {
 			this.getLogger().severe("Error creating marker set");
+			this.getPluginLoader().disablePlugin(this);
 			return;
 		}
 
-		this.world = this.getServer().getWorlds().get(cfg.getInt("world"));
-		if (world == null) {
-			this.getLogger().severe("No world found!");
-			return;
-		}
+		int minzoom = cfg.getInt("minimumZoom", 0);
+		if (minzoom > 0) markerSet.setMinZoom(minzoom);
+		markerSet.setLayerPriority(cfg.getInt("priority", 12));
+		markerSet.setHideByDefault(cfg.getBoolean("hideByDefault", true));
 
-		FeatureCollection<SimpleFeatureType, SimpleFeature> features;
-		try {
-			features = featureSource.getFeatures();
-		} catch (IOException e) {
-			e.printStackTrace();
-			return;
-		}
+		for (String section : cfg.getConfigurationSection("shapefiles").getKeys(false)) {
+			section = "shapefiles." + section;
+			this.scaling = cfg.getDouble(section + "." + "scaling", 120);
+			this.xOffset = cfg.getInt(section + "." + "xOffset", 0);
+			this.y = cfg.getInt(section + "." + "y", 64);
+			this.zOffset = cfg.getInt(section + "." + "zOffset", 0);
 
-		FeatureIterator<SimpleFeature> iterator = features.features();
-		int iteration = -1;
-		while (iterator.hasNext()) {
-			iteration++;
-			SimpleFeature feature = iterator.next();
+			File shapefile = new File(this.getDataFolder(),
+					this.getConfig().getString(section + "." + "shapefilePath"));
+			if (shapefile == null || !shapefile.isFile()) {
+				this.getLogger().warning("Shapefile not found!");
+				this.getPluginLoader().disablePlugin(this);
+				return;
+			}
 
-			// name of area
-			// formerly index 4, which was the semi-governing nation over it (Guam -> USA)
-			String countryName = (String) feature.getAttribute(8);
+			FileDataStore store = FileDataStoreFinder.getDataStore(shapefile);
 
-			int index = 0;
+			this.featureSource = store.getFeatureSource();
 
-			for (Property property : feature.getProperties()) {
-				index++;
-				String propertyValue = property.getValue().toString();
-				if (!propertyValue.contains("(((")) continue;
+			this.world = this.getServer().getWorlds().get(cfg.getInt(section + "." + "world"));
+			if (world == null) {
+				this.getLogger().severe("No world found!");
+				this.getPluginLoader().disablePlugin(this);
+				return;
+			}
 
-				String[] polygons = { propertyValue };
-				if (propertyValue.contains("), (")) {
-					polygons = propertyValue.split(Pattern.quote("), ("));
-				}
+			FeatureCollection<SimpleFeatureType, SimpleFeature> features;
+			try {
+				features = featureSource.getFeatures();
+			} catch (IOException e) {
+				e.printStackTrace();
+				this.getPluginLoader().disablePlugin(this);
+				return;
+			}
+			FeatureIterator<SimpleFeature> iterator = features.features();
 
-				int polygonIndex = 0;
-				for (String polygon : polygons) {
-					String id = countryName + "_" + iteration + "_" + index + "_" + polygonIndex;
-					polygon = polygon.replace("MULTIPOLYGON ", "").replace("(", "").replace(")", "");
-					String[] locations = polygon.split(", ");
+			int iteration = -1;
+			try {
+				while (iterator.hasNext()) {
+					iteration++;
+					SimpleFeature feature = iterator.next();
 
-					double[] x = new double[locations.length];
-					double[] y = new double[locations.length];
-					double[] z = new double[locations.length];
+					int index = 0;
 
-					int i = 0;
-					for (String location : locations) {
-						String[] coords = location.split(" ");
-						double lat = 0;
-						double lon = 0;
-						try {
-							lat = Double.valueOf(coords[0]);
-							lon = Double.valueOf(coords[1]);
-						} catch (NumberFormatException e) { 
-							e.printStackTrace();
-							continue;
+					for (Property property : feature.getProperties()) {
+						index++;
+						String propertyValue = property.getValue().toString();
+						if (!propertyValue.contains("(((")) continue;
+
+						String[] polygons = { propertyValue };
+						if (propertyValue.contains("), (")) {
+							polygons = propertyValue.split(Pattern.quote("), ("));
 						}
-						x[i] = (lat * this.scaling) + xOffset;
 
-						y[i] = this.y;
+						int polygonIndex = 0;
+						for (String polygon : polygons) {
+							String id = iteration + "_" + index + "_" + polygonIndex;
+							polygon = polygon.replace("MULTIPOLYGON ", "").replace("(", "").replace(")", "");
+							String[] locations = polygon.split(", ");
 
-						z[i] = (lon * this.scaling) * -1 + zOffset;
-						i++;
+							double[] x = new double[locations.length];
+							double[] y = new double[locations.length];
+							double[] z = new double[locations.length];
+
+							int i = 0;
+							for (String location : locations) {
+								String[] coords = location.split(" ");
+								double lat = 0;
+								double lon = 0;
+								try {
+									lat = Double.valueOf(coords[0]);
+									lon = Double.valueOf(coords[1]);
+								} catch (NumberFormatException e) {
+									e.printStackTrace();
+									continue;
+								}
+								x[i] = (lat * this.scaling) + xOffset;
+
+								y[i] = this.y;
+
+								z[i] = (lon * this.scaling) * -1 + zOffset;
+								i++;
+							}
+
+							if (markerSet.findPolyLineMarker(id) != null) markerSet.findPolyLineMarker(id).deleteMarker();
+
+							PolyLineMarker polyline = markerSet.createPolyLineMarker(id,
+									"", false, world.getName(), x, y, z, false);
+							if(polyline == null) {
+								this.getLogger().info("Error adding polyline " + id);
+								continue;
+							}
+
+							int color = cfg.getInt(section + "." + "style.color", 0xCC66CC);
+							polyline.setLineStyle(cfg.getInt(section + "." + "style.lineThickness", 3),
+									cfg.getDouble(section + "." + "style.lineOpacity", .5), color);
+
+							polygonIndex++;
+						}
 					}
-
-					if (markerSet.findPolyLineMarker(id) != null) markerSet.findPolyLineMarker(id).deleteMarker();
-
-					PolyLineMarker polyline = markerSet.createPolyLineMarker(id, 
-							countryName, false, world.getName(), x, y, z, false);
-					if(polyline == null) {
-						this.getLogger().info("error adding area marker " + id);
-						continue;
-					}
-
-					int color = cfg.getInt("style.color", 0xCC66CC);
-					polyline.setLineStyle(cfg.getInt("style.lineThickness", 3), cfg.getDouble("style.lineOpacity", .5), color);
-
-					polygonIndex++;
 				}
+				iterator.close();
+			} catch (Exception e) { // can happen from a bad cast or something
+				e.printStackTrace();
+				iterator.close();
+				this.getPluginLoader().disablePlugin(this);
+				return;
 			}
 		}
-		iterator.close();
 
-		int minzoom = cfg.getInt("style.minimumZoom", 0);
-		if (minzoom > 0) markerSet.setMinZoom(minzoom);
-		markerSet.setLayerPriority(cfg.getInt("style.priority", 12));
-		markerSet.setHideByDefault(cfg.getBoolean("style.hideByDefault", true));
-
-		if (cfg.getBoolean("style.enableCountryMarkers", true)) handleCountryMarkers();
-
+		if (cfg.getBoolean("enableCountryMarkers", true)) handleCountryMarkers();
 		this.getLogger().info("Version " + this.getDescription().getVersion() + " is activated!");
 	}
 
-	// only run after activate()
+	/**
+	 * Handles country markers
+	 * Only run after activate()
+	 * @throws IOException
+	 */
 	public void handleCountryMarkers() throws IOException {
-		Reader reader = this.getTextResource(this.getConfig().getString("countriesFile"));
+		Reader reader = this.getTextResource("countries.txt");
 		if (reader == null) {
 			this.getLogger().warning("Countries file not found. Country markers not loaded.");
 			return;
@@ -215,7 +261,7 @@ public class DynmapCountries extends JavaPlugin {
 			double z = Double.valueOf(separated[1]) * this.scaling * -1;
 
 			markerSet.createMarker(separated[0], separated[3], this.world.getName(), x, this.y, z, 
-					markerapi.getMarkerIcon(this.getConfig().getString("style.markerIcon", "king")), false);
+					markerapi.getMarkerIcon(this.getConfig().getString("markerIcon", "king")), false);
 		}
 	}
 
